ae0664f54814f63f7f5abe0fa4fd6148682ff982
my1977 <1977905246@qq.com>
2016-03-02 15:11:01 +0800

    线下模拟用户数据

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -2,6 +2,25 @@
 
 //检查微信授权
 function checkWeixinAuth(){
+    //线下模拟用户数数据
+    if ($_SERVER['HTTP_HOST'] != 'dq1yt.ztpet.cn') {
+        $_SESSION['me']['weixin'] = array(
+            'me' => array(
+                'weixin' => array(
+                    'openid'    => 'ojkF1s0zSD-UINuV1So6NlhtAzFw',
+                    'nickname'  => '马莹',
+                    'sex'       => 1,
+                    'language'  => 'zh_CN',
+                    'city'      => '吉林',
+                    'province'  => '吉林',
+                    'country'   => '中国',
+                    'headimgurl' => 'http://wx.qlogo.cn/mmopen/uOleZXC9MkHt18ILqqRobph0G6XHjPFicCYWMRDAZSW3vbDFNWQnT5sg2Hk3ADz6pJfpkrA4gKVbvBLwcicsvOBuOPOgrIfnwK/0',
+                    'privilege'  => array()
+                )
+            )
+        );
+    }
+
     if (!$_SESSION['me']['weixin']) {
         $redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['QUERY_STRING'];
         $Weixin = new \Common\Util\WeixinAuth();
